69ddacdde4a6c3424f2ceb99de4c693fe1063c03
Odi000 <mandimandi.ro@gmail.com>
2022-09-19 16:04:57 +0200

    Little change to the last input

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const patterns = {
     telephone: /^06[7-9]\d{7}$/,
     username: /^[a-z\d]{5,12}$/i,
     password: /^[\w@-]{8,20}$/,
-    amesia: /^[a-z ]{3,25}$/i,
+    company_name: /^[a-z]{3,25}$/i,
     email: /^([a-z\d\.-_]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 }
 
@@ -19,7 +19,7 @@ function validate(field, regEx){
 
 inputs.forEach((input) =>{
     input.addEventListener('keyup',(e) =>{
-        //console.log(e.target.attributes.name.value);
-        validate(e.target, patterns[e.target.attributes.name.value])
+        //console.log(e.target.attributes.type.value);
+        validate(e.target, patterns[e.target.attributes.name.value]);
     });
 });
